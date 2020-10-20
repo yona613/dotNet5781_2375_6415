9997234f4030bc16c01036af4a6443e4cd45fe19
Yona Szmerla <yszmerla@g.jct.ac.il>
2020-10-20 23:24:54 +0300

    initiate + Welcome2375

---
+++ dotNet5781_ 2375_6415/dotNet5781_00_ 2375_6415/Program2375.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00__2375_6415
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2375();
+            Welcome6415();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome6415();
+
+        private static void Welcome2375()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
