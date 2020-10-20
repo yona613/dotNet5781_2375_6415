12af49acda84dba428b8a61b621eef7abd701ff3
אליסף דימנט <edimant@g.jct.ac.il>
2020-10-20 23:39:57 +0300

    +Welcome6415

---
+++ dotNet5781_ 2375_6415/dotNet5781_00_ 2375_6415/Program6415.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00__2375_6415
+{
+    partial class Program
+    {
+        static partial void Welcome6415()
+        {
+            Console.WriteLine("I am also here");
+        }
+        
+    }
+}
