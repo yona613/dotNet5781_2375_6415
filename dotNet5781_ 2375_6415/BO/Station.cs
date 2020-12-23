c8b112919283436a1021fd05af9460fe8669b221
אליסף דימנט <edimant@g.jct.ac.il>
2020-12-23 21:13:02 +0200

    elyasaf BO

---
+++ dotNet5781_ 2375_6415/BO/Station.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+
+namespace BO
+{
+    public class Station
+    {
+        public int StationId { get; set; }
+
+        public  GeoCoordinate Coordinates { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public bool  Invalid { get; set; }
+
+        public bool Roof { get; set; }
+
+        public bool DigitalPanel { get; set; }
+    }
+}
---
+++ dotNet5781_ 2375_6415/BO/UserTrip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class UserTrip
+    {
+        public string UserName { get; set; }
+
+        public int LineNumber { get; set; }
+
+        public int FirstStation { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public int LastStation { get; set; }
+
+        public DateTime StopTime { get; set; }
+
+    }
+}
