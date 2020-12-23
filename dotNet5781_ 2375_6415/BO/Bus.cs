958ddbfb03b7e15d9827bd01856d05cff1ab5a74
Yona Szmerla <yszmerla@g.jct.ac.il>
2020-12-23 21:02:20 +0200

    BO started

---
+++ dotNet5781_ 2375_6415/BO/Bus.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Bus
+    {
+        private int license;
+
+        public int License
+        {
+            get { return license; }
+            set { license = value; }
+        }
+
+        private DateTime licenseDate;
+
+        public DateTime LicenseDate
+        {
+            get { return licenseDate; }
+            set { licenseDate = value; }
+        }
+
+        private int kilometrage;
+
+        public int Kilometrage
+        {
+            get { return kilometrage; }
+            set { kilometrage = value; }
+        }
+
+        private int fuel;
+
+        public int Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+
+        private Status busStatus;
+
+        public Status BusStatus
+        {
+            get { return busStatus; }
+            set { busStatus = value; }
+        }
+
+        private DateTime testDate;
+
+        public DateTime TestDate
+        {
+            get { return testDate; }
+            set { testDate = value; }
+        }
+
+        private int kmFromTest;
+
+        public int KmFromTest
+        {
+            get { return kmFromTest; }
+            set { kmFromTest = value; }
+        }
+
+        private string brand;
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        private bool airConditionning;
+
+        public bool AirConditionning
+        {
+            get { return airConditionning; }
+            set { airConditionning = value; }
+        }
+
+        private Activity myActivity;
+        public Activity MyActivity
+        {
+            get { return myActivity; }
+            set { myActivity = value; }
+        }
+    }
+}
